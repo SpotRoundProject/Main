2233be152427d1fed16d0b75243005f05b3884d2
chetanpukale <dattatrayapukale17@gmail.com>
2021-10-30 10:45:10 +0530

    hi

---
+++ app/src/main/java/com/example/spotround/modle/Utils.java
@@ -0,0 +1,9 @@
+package com.example.spotround.modle;
+public class Utils {
+
+    //This is your from email
+    public static final String EMAIL = "spotroundapp@gmail.com";
+
+    //This is your from email password
+    public static final String PASSWORD = "SubCheAbh@123";
+}
